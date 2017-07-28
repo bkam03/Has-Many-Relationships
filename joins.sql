05d6ca8e6f78a1a20de888a178e2d0466240127c
BK <bkam03@yahoo.com>
2017-07-27 19:02:46 -1000

    query 9 completed

--- joins.sql
+++ joins.sql
@@ -72,7 +72,13 @@ SELECT comments.*, posts.title AS post_title, posts.url AS post_url, comments.bo
 
 --9Create a query to get the all rows in the comments table, showing post title (aliased as post_title), post url (ailased as post_url), and the comment body (aliased as comment_body) where the post was created after January 1, 2015
 
-
+SELECT comments.*, posts.tiTle AS post_title, posts.url AS post_url, comments.body AS comment_body
+  FROM
+    comments
+    INNER JOIN
+    posts
+    ON comments.post_id = posts.id
+  WHERE posts.created_at > '1-1-2015';
 
 --10 Create a query to get the all rows in the comments table, showing post title (aliased as post_title), post url (ailased as post_url), and the comment body (aliased as comment_body) where the comment body contains the word 'USB'
 
